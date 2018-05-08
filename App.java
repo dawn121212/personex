a6f6f566d454e41963c629f953a1b8c8830281e1
Amy <ammz1212@MacBook-Pro.local>
2018-05-07 22:05:40 -0400

    Initial commit

---
+++ App.java
@@ -0,0 +1,33 @@
+
+class App
+{
+    public static void main(String [] args)
+    
+    {
+        
+        //int height; can't add height here, it must be in the class Person. it is like in the book 
+                       // there is not height in the table of content, you just stick a article with height
+                       // inside the book
+        
+        
+        
+          Person person1 = new Person();
+          person1.name= "John";
+          person1.age = 34;
+          //person1.height= 5;   This won't work
+          person1.speak();
+          
+    
+    
+    
+    
+    }
+
+
+
+}
+
+
+
+
+
---
+++ Person.java
@@ -0,0 +1,18 @@
+class Person
+{
+String name;
+int age;
+ 
+
+void speak()
+{
+ System.out.println("My name is  " + name + "," + " and age is "  +  age );
+
+}
+
+
+
+
+
+
+}
